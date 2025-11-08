deb876c78d4e6a5bab5c78c58ba7b9e53cf05fe3
glitchcore <mail@s3f.ru>
2025-11-08 23:46:11 +0400

    get include path for libdeps

--- builder/frameworks/arduino.py
+++ builder/frameworks/arduino.py
@@ -42,3 +42,32 @@ if isfile(PREBUILT_LIB):
 else:
     print("Warning: Pre-compiled library not found at: %s" % PREBUILT_LIB)
     print("Please run: cd urack-platform && python3 build_precompiled_libs.py")
+
+# Add prebuilt libraries include paths for external libraries
+# Each library directory needs to be added to CPPPATH so headers can be found
+PREBUILT_LIBRARIES_DIR = join(PREBUILT_DIR, "include", "libraries")
+if os.path.isdir(PREBUILT_LIBRARIES_DIR):
+    library_paths = []
+    # Add each library directory to CPPPATH (standard PlatformIO behavior)
+    for item in os.listdir(PREBUILT_LIBRARIES_DIR):
+        lib_path = join(PREBUILT_LIBRARIES_DIR, item)
+        if os.path.isdir(lib_path):
+            # Check if library directory has any header files
+            has_headers = any(f.endswith(('.h', '.hpp', '.inc')) for f in os.listdir(lib_path) if os.path.isfile(join(lib_path, f)))
+            
+            if has_headers:
+                library_paths.append(lib_path)
+                env.Prepend(CPPPATH=[lib_path])
+            else:
+                print("Warning: Library directory %s exists but has no header files. Please rebuild libraries." % item)
+    
+    if library_paths:
+        print("URack Arduino: Added %d prebuilt library include paths:" % len(library_paths))
+        for lib_path in library_paths:
+            print("  - %s" % lib_path)
+    else:
+        print("Warning: No library directories with headers found in: %s" % PREBUILT_LIBRARIES_DIR)
+        print("Please run: cd urack-platform && python3 build_precompiled_libs.py")
+else:
+    print("Warning: Prebuilt libraries directory not found at: %s" % PREBUILT_LIBRARIES_DIR)
+    print("Please run: cd urack-platform && python3 build_precompiled_libs.py")
